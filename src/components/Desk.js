006dcc0317605ce36c8d244545706c1ba7a898ca
etblh <hugo432121@gmail.com>
2019-11-11 19:38:39 +0800

    no change

--- src/components/Desk.js
+++ src/components/Desk.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useStore} from 'reto';
+import Draggable from 'react-draggable';
 import Store from '../store'
 import './Desk.scss'
 
@@ -7,14 +8,16 @@ const Desk = (props) => {
     const store = useStore(Store);
 
     return (
-        <div className='desk'>
-            {/* <a href={()=>{}}>Amount</a> */}
-            {
-                store.DH_Table[props.ID].human.map((name,index) => {
-                    return <div className='human' key={index}>{name}</div>
-                })
-            }
-        </div>
+        <Draggable bounds="parent">
+            <div className='desk'>
+                {/* <a href={()=>{}}>Amount</a> */}
+                {
+                    store.DH_Table[props.ID].human.map((name,index) => {
+                        return <div className='human' key={index}>{name}</div>
+                    })
+                }
+            </div>
+        </Draggable>
     )
 }
 
